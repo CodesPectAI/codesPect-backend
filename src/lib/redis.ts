e9279eaf9ef0207b3ed0afdc09e2c5732c43bc88
vishwajeet-8 <vishwajeetrout8@gmail.com>
2026-08-13 01:19:53 +0530

    configuration for deploying

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,6 @@
 import { Redis } from "ioredis";
+import "dotenv/config";
 
-export const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+export const redis = new Redis(process.env.REDIS_url!, {
   maxRetriesPerRequest: null,
 });
